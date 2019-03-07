f7b822d6cd2cbd25bf41567ef257632eb64128ba
David Bobb <david.bobb@txauus-ml-0003115.local>
2019-03-06 20:57:13 -0600

    to do jquery

--- practice_jquery/todo.js
+++ practice_jquery/todo.js
@@ -1,4 +1,20 @@
-$(document).ready(function($) {
-    alert("js is working");
-}
-)
+$(function() {
+
+    let $list = $('ul');
+    let $newItemForm = $('#newItemForm');
+    let $close = $('.close');
+
+    $newItemForm.on('submit', function(e) {
+        e.preventDefault();
+        let text = $('input[type="text"]').val();
+        $list.append(`<li>${text}<span class="close">-</span></li>`);
+        $('input[type="text"]').val('');
+    });
+    
+    $list.on('click', 'li', function() {
+    let $this = $(this);
+    $this.addClass('checked');
+    });
+
+    
+});
